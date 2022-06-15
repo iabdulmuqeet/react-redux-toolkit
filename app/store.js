863c50fccd8e9cd74c7f3be0c3ae0e9b9f2354bb
Abdul Muqeet Arshad <abdulmuqeet.arshad@devsinc.com>
2022-06-15 16:38:40 +0500

    added logger middleware

--- app/store.js
+++ app/store.js
@@ -1,10 +1,14 @@
 const { configureStore } = require('@reduxjs/toolkit');
+const { createLogger } = require('redux-logger');
 const cakeReducer = require('../features/cake/cakeSlice');
+
+const logger = createLogger();
 
 const store = configureStore({
 	reducer: {
 		cake: cakeReducer,
 	},
+	middleware: getDefaultMiddleware => getDefaultMiddleware().concat(logger),
 });
 
 module.exports = store;
